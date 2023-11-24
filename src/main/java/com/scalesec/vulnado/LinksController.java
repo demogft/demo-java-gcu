f69e5648c749c08ea46a3caa481543c9784d7d4d
Julio Arruda <julio.arruda@outlook.com>
2023-11-24 15:21:36 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinksController.java

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,16 +1,10 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
 import java.util.List;
-import java.io.Serializable;
 import java.io.IOException;
 
 
 @RestController
-@EnableAutoConfiguration
 public class LinksController {
   @RequestMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
