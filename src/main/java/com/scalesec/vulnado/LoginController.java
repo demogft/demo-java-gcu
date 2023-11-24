68fff1c4a87a7e39ce17e2db7c77c3f21b4c3787
Julio Arruda <julio.arruda@outlook.com>
2023-11-24 16:15:35 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.*;
 import java.io.Serializable;
 
@@ -12,10 +11,10 @@ import java.io.Serializable;
 @EnableAutoConfiguration
 public class LoginController {
   @Value("${app.secret}")
-  private String secret;
+  private final String secret;
 
   @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @PostMapping(value = "/login", produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);
     if (Postgres.md5(input.password).equals(user.hashedPassword)) {
