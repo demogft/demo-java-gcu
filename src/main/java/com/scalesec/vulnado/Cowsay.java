8136b7f1b50c0a317da84bac2f368c22e73cbec4
Julio Arruda <julio.arruda@outlook.com>
2023-11-24 16:16:09 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,12 +2,19 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 public class Cowsay {
+  private static final Logger logger = Logger.getLogger(Cowsay.class.getName());
+
+  private Cowsay() {
+    //This constructor is intentionally empty.
+  }
+
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    logger.info(cmd);
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();
