8d52d86c9dba5f78da102938cb2b07ee4534abf2
Julio Arruda <julio.arruda@outlook.com>
2023-11-24 16:15:17 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinksController.java

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,13 +1,10 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
-import org.springframework.http.HttpStatus;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import java.util.List;
-import java.io.Serializable;
-import java.io.IOException;
-
 
 @RestController
 @EnableAutoConfiguration
