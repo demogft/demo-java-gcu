b417c00e304a7fa8a5054d294f733aa90ff1069a
Julio Arruda <julio.arruda@outlook.com>
2023-11-24 16:14:30 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Postgres.java

--- src/main/java/com/scalesec/vulnado/Postgres.java
+++ src/main/java/com/scalesec/vulnado/Postgres.java
@@ -89,9 +89,7 @@ public class Postgres {
 
     private static void insertUser(String username, String password) {
        String sql = "INSERT INTO users (user_id, username, password, created_on) VALUES (?, ?, ?, current_timestamp)";
-       PreparedStatement pStatement = null;
-       try {
-          pStatement = connection().prepareStatement(sql);
+       try(PreparedStatement pStatement = connection().prepareStatement(sql)) {
           pStatement.setString(1, UUID.randomUUID().toString());
           pStatement.setString(2, username);
           pStatement.setString(3, md5(password));
@@ -103,9 +101,7 @@ public class Postgres {
 
     private static void insertComment(String username, String body) {
         String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?, ?, ?, current_timestamp)";
-        PreparedStatement pStatement = null;
-        try {
-            pStatement = connection().prepareStatement(sql);
+        try(PreparedStatement pStatement = connection().prepareStatement(sql)) {
             pStatement.setString(1, UUID.randomUUID().toString());
             pStatement.setString(2, username);
             pStatement.setString(3, body);
